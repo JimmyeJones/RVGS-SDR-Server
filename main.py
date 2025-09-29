a4d9a43ce12fa377b6845fc09c4558f066ad0291
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-09-29 07:42:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import streamlit as st
 st.title("Software Defined Radio Server")
+col1, col2, col3, col4 = st.columns(4)
+if col1.button("Home"):
+  page = "home"
+if col2.button("Connect"):
+  page = "connect"
+if col3.button("Satellite Image Viewer"):
+  page = "image_viewer"
+if col4.button("About"):
+  page = "about"
 def homepage():
   st.subheader("Roanoke Valley Governor's School")
+def connect():
+  st.title("Connect")
+def image_viewer():
+  
 
