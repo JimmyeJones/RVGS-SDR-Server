909390c9e2ba29df7458e37dabfc56c02ddc8a72
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-09-29 11:26:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ col1.image("https://resources.finalsite.net/images/f_auto,q_auto,t_image_size_2/
 col3.subheader("Roanoke Valley Governor's School")
 
 col1, col2, col3 = st.columns([7,8,7])
-col2.subheader("Software Defined Radio Server")
+col2.write('<p style="font-size:26px;">Software Defined Radio Server</p>',
 
 col1, col2, col3, col4 = st.columns(4)
 if col1.button("Home", width="stretch"):
