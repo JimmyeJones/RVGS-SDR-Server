c5149952e2ec237adec86cef0bd0a635ec1a7ee9
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-09-30 07:41:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ col1.image("RVGSBanner.webp")
 col1.write('''<p style="font-size:15px;">Roanoke Valley Governor's School</p>''', unsafe_allow_html=True)
 col3.write('<p style="font-size:25px;">Software Defined Radio Server</p>', unsafe_allow_html=True)
 
+if 'page' not in st.session_state:
+    st.session_state['page'] = 'home'
+
 col1, col2, col3, col4 = st.columns(4)
 if col1.button("Home", width="stretch"):
-  page = "home"
+  st.session_state['page'] = 'home'
 if col2.button("Connect", width="stretch"):
-  page = "connect"
+  st.session_state['page'] = 'connect'
 if col3.button("Satellite Viewer", width="stretch"):
-  page = "image_viewer"
+  st.session_state['page'] = 'image_viewer'
 if col4.button("About", width="stretch"):
-  page = "about"
-try:
-  str(page)
-except NameError:
-  page = "home"
-st.text(page)
+  st.session_state['page'] = 'about'
+
+st.text(st.session_state['page'])
 st.button("test")
 def homepage():
   st.subheader("Roanoke Valley Governor's School")
